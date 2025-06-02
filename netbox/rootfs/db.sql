ed7b781795e849aeecf18811ada1812ed798fc2a
Mike Splain <mike.splain@gmail.com>
2025-06-02 09:46:12 -0400

    Cleanup

--- netbox/rootfs/db.sql
+++
@@ -1,7 +0,0 @@
--- https://docs.netbox.dev/en/stable/installation/1-postgresql/#database-creation
-CREATE DATABASE netbox;
-CREATE USER netbox WITH PASSWORD 'netbox';
-ALTER DATABASE netbox OWNER TO netbox;
--- the next two commands are needed on PostgreSQL 15 and later
-\connect netbox;
-GRANT CREATE ON SCHEMA public TO netbox;
